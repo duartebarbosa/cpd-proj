03b276256f0a5fedc2ab8b99ad5d12a17e18f842
Duarte Barbosa <duarte.barbosa@gmail.com>
2012-11-26 23:53:16 +0000

    major improvements; speedup equals to roughly 2

--- docs-omp2.c
+++ docs-omp2.c
@@ -18,9 +18,44 @@ struct {
 	FILE *out;
 } info;
 
+inline int power(int b, int e){
+	register int res = 1;
+	for(; e != 0; e--)
+		res *= b;
+	return res;
+}
+
+double naive_strtod(const char *p) {
+    double r = 0.0;
+    int neg = 0;
+    if (*p == '-') {
+        neg = 1;
+        ++p;
+    }
+    while (*p >= '0' && *p <= '9') {
+        r = (r*10.0) + (*p - '0');
+        ++p;
+    }
+    if (*p == '.') {
+        double f = 0.0;
+        int n = 0;
+        ++p;
+        while (*p >= '0' && *p <= '9') {
+            f = (f*10.0) + (*p - '0');
+            ++p;
+            ++n;
+        }
+        r += f / power(10.0, n);
+    }
+    if (neg)
+        return -r;
+    
+    return r;
+}
+
 int init(char * filename){
 	register int sub, doc;
-	char * token = NULL, line[LINE_SIZE] = {0};
+	char *tmp = NULL, line[LINE_SIZE] = {0};
 	
 	if((info.in = fopen(filename, "r")) == NULL)
 		return -2;
@@ -28,16 +63,19 @@ int init(char * filename){
 	if(fscanf(info.in, "%d\n %d\n %d\n", &info.cabinet, &info.document, &info.subject) != 3)
 		return -4;
 	
-	info.score = (double **) calloc(info.document, sizeof(double*));
-	info.cabinets = (int *) calloc(info.document, sizeof(int));
+	info.cabinets = malloc(info.document*sizeof(int));
+	info.score = malloc(info.document*sizeof(double*));
+
+	#pragma omp parallel for
+	for(doc = 0; doc < info.document; doc++)
+		info.cabinets[doc] = doc % info.cabinet;
 
 	for(doc = 0; doc < info.document; doc++){
-		info.cabinets[doc] = doc % info.cabinet;
-		info.score[doc] = (double *) calloc(info.subject, sizeof(double));
-		if(fgets(line, LINE_SIZE, info.in)!= NULL){
-			strtok(line, " ");
-			for(sub = 0; sub < info.subject && (token = strtok(NULL, " ")) != NULL; sub++)
-				info.score[doc][sub] = atof(token);
+		info.score[doc] = malloc(info.subject*sizeof(double));
+		if(fgets(line, LINE_SIZE, info.in)){
+			strtok_r(line, " ", &tmp);
+			for(sub = 0; sub < info.subject; sub++)
+				info.score[doc][sub] = naive_strtod(strtok_r(NULL, " ", &tmp));
 		}
 	}	
 	return 0;
@@ -46,11 +84,11 @@ int init(char * filename){
 int process(){
 	register int sub, doc, cab, tmp, flag = 1;
 	register double distance, aux;
-	int *docPerCab = calloc(info.cabinet, sizeof(int)); 		/* docPerCab[cabinet] */
-	double **centroid = calloc(info.subject, sizeof(double*));	/* centroid[cabinet][subject] - centroid of the cabinet */
+	int *docPerCab = malloc(info.cabinet*sizeof(int)); 		/* docPerCab[cabinet] */
+	double **centroid = malloc(info.subject*sizeof(double*));	/* centroid[cabinet][subject] - centroid of the cabinet */
 
 	for(sub = 0; sub < info.subject; sub++)
-		centroid[sub] = calloc(info.cabinet, sizeof(double));
+		centroid[sub] = malloc(info.cabinet*sizeof(double));
 
 	while(flag){
 		flag = 0;
@@ -100,7 +138,7 @@ int process(){
 
 int flushClean(char *filename){
 	register int doc = 0;
-	char *outfile = alloca(strlen(filename));
+	char *outfile = alloca(strlen(filename)+1);
 	outfile = strcat(strtok(strcpy(outfile, filename), "."), ".out");
 
 	if(fclose(info.in) == EOF)
